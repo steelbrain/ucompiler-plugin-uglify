05563146cfcc204ab96804cc5cc5f0b1c681fdf1
steelbrain <aneesiqbalbhatti@gmail.com>
2015-11-21 10:19:06 -0700

    :art: Fix some minor bugs

--- src/main.js
+++ src/main.js
@@ -7,15 +7,15 @@ export const minifier = false
 export function process({contents, file, config, state}) {
   const beginning = contents.substr(0, 11)
   if (beginning !== '"use babel"' && beginning !== "'use babel'") {
-    return {contents, sourceMap: null}
+    return contents
   }
-  config.babel = config.babel || {}
 
   const transpiled = transform(contents, Object.assign({}, config.babel, {
     filename: file,
     filenameRelative: file,
-    sourceRoot: config.root,
-    sourceMaps: true
+    sourceRoot: state.root,
+    sourceMaps: true,
+    highlightCode: false
   }))
   state.sourceMap = transpiled.map.toString()
   return transpiled.code
